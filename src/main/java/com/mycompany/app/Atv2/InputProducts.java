57e4aab803dbe7cf9d486b45047853bfee76d62a
AbraaoAlves <abraao.teodosio@gmail.com>
2023-06-04 18:56:22 +0000

    feat: InputProducts
    - implement leia method and construct a single Entrada

--- src/main/java/com/mycompany/app/Atv2/InputProducts.java
+++ src/main/java/com/mycompany/app/Atv2/InputProducts.java
@@ -1,10 +1,25 @@
 package com.mycompany.app.Atv2;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class InputProducts {
+  static Scanner scan = new Scanner(System.in);
+  
+  public static  double leia(String message) {
+    System.out.println(message);
+    return scan.nextDouble();
+  } 
 
   public static List<Entrada> getEntradas() {
+    
+    int ID = (int)leia("Digite o ID do produto: ");
+    double value = leia("Digite o Valor do produto: ");
+    double percent = leia("Digite o Percentual de aumento do produto: ");
+
+    Entrada entrada = new Entrada(ID, value, percent);
+
+
     return null;
   }
 
