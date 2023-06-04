28881687ed6f522d4094b8081e65fd5eac507b55
AbraaoAlves <abraao.teodosio@gmail.com>
2023-06-04 18:55:55 +0000

    chore: implment Entrada with constructor

--- src/main/java/com/mycompany/app/Atv2/Entrada.java
+++ src/main/java/com/mycompany/app/Atv2/Entrada.java
@@ -1,5 +1,28 @@
 package com.mycompany.app.Atv2;
 
 public class Entrada {
+  //  * ID, Valor e Percentual de aumento
+  
+  private int ID;
+  public int getID() {
+    return ID;
+  }
+
+  private double value;
+  public double getValue() {
+    return value;
+  }
+
+  private double risePercent;
+
+  public double getRisePercent() {
+    return risePercent;
+  }
+
+  public Entrada(int id, double value, double percent){
+    this.ID = id;
+    this.value = value;
+    this.risePercent = percent;
+  }
 
 }
