33270b2e3b96cc95a53fcea4f57019771816063c
AbraaoAlves <abraao.teodosio@gmail.com>
2023-06-04 18:40:19 +0000

    chore: pseudocode

--- src/main/java/com/mycompany/app/Atv2/App3.java
+++ src/main/java/com/mycompany/app/Atv2/App3.java
@@ -1,5 +1,7 @@
 package com.mycompany.app.Atv2;
 
+import java.util.List;
+
 /**
  * 
  * Uma empresa realizou uma pesquisa com os seus fornecedores, na qual foram coletados os seguintes dados referentes aos produtos fornecidos:
@@ -18,6 +20,35 @@ package com.mycompany.app.Atv2;
  */
 public class App3 {
   public static void main(String[] args){
+      
       System.out.println("Ola qualquer coisa");
+      // o programa solicita dados até que o ID digitado seja zero
+      // ele deve devolver uma lista de entradas
+      //List<Entradas> entradas = InputProducts.getProdutcs();
+//
+      //ProductsInfo info = new ProductsInfo(entradas);
+//
+      ////O novo valor de cada um dos produtos. 
+      //List<Produto> produtos = info.getProdutos();
+      //
+      //System.out.println("| ID | Value |");
+      //for (Produto produto : produtos) {
+      //  System.out.printf("| %s | R$ %s |", produto.id, produto.value);
+      //}
+//
+      //// A quantidade de produtos mais caros que R$ 100,00 (após aumento) e que tiveram aumento superior a 5%.
+      //int quantidade = info.expensiveProducts().size();
+      //System.out.println("A quantidade de produtos mais caros que R$ 100,00 (após aumento) e que tiveram aumento superior a 5%, foi: ");
+      //System.out.printf("%s", quantidade);
+//
+      //// A média de valor dos produtos que não sofreram aumento.
+      //double media = info.averageProductsWithoutRise();
+      //System.out.println("A média de valor dos produtos que não sofreram aumento, foi: ");
+      //System.out.printf("%s", media);
+//
+      ////O valor do produto mais caro (após aumento)
+      //Produto produto = info.expensiveProduct();
+      //System.out.println("O valor do produto mais caro (após aumento), foi: ");
+      //System.out.printf("%s - R$ %s", produto.id, produto.value);
   }
 }
