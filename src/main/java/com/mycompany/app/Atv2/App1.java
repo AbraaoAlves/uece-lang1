5f82a64bb52b500f6b53d687cc5f92bd157f1945
AbraaoAlves <abraao.teodosio@gmail.com>
2023-06-04 16:04:46 +0000

    chore: enuciado da questão1

--- src/main/java/com/mycompany/app/Atv2/App1.java
+++ src/main/java/com/mycompany/app/Atv2/App1.java
@@ -1,5 +1,20 @@
 package com.mycompany.app.Atv2;
 
+/**
+ * 
+ * 1. Um Banco concederá um crédito especial aos seus clientes, variável com o saldo médio no último ano. Faça um algoritmo que leia o saldo médio de um cliente e calcule o valor do crédito de acordo com a tabela abaixo.
+ * 
+ *  Mostre uma mensagem informando o saldo médio e o valor do crédito.
+ * 
+ *  saldo medio | Percentual
+ * 
+ * 0 a 200      | nenhum credito
+ * 201 a 400    | 20% do valor do saldo medio
+ * 401 a 600    | 30% do valor do saldo medio
+ * acima de 600 | 40% do valor do saldo medio
+ * 
+ */
+
 public class App1 {
   public static void main(String[] args) {
     System.out.println("Ola mundo 2");
