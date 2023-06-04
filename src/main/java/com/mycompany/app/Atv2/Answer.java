4e21c1feb92b6341092833f9b504b2d283ca1574
AbraaoAlves <abraao.teodosio@gmail.com>
2023-06-04 22:23:13 +0000

    feat: get random answer data

--- src/main/java/com/mycompany/app/Atv2/Answer.java
+++ src/main/java/com/mycompany/app/Atv2/Answer.java
@@ -1,5 +1,29 @@
 package com.mycompany.app.Atv2;
 
+/**
+ * idade da pessoa; 
+ * o identificador da pessoa; 
+ * opinião em relação à peça(de 0 a 10).
+ */
 public class Answer {
+  private int ID;
+  public int getID() {
+    return ID;
+  }
+  private int age;
+  public int getAge() {
+    return age;
+  }
+  private int hate;
+  public int getHate() {
+    return hate;
+  }
 
+  Answer(int id, int age, int hate){
+    this.ID = id;
+    this.age = age;
+    this.hate = hate;
+  }
+
+  
 }
--- src/main/java/com/mycompany/app/Atv2/InputQuestions.java
+++ src/main/java/com/mycompany/app/Atv2/InputQuestions.java
@@ -1,11 +1,23 @@
 package com.mycompany.app.Atv2;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class InputQuestions {
 
   public static List<Answer> getAnswers() {
-    return null;
+    List<Answer> result = new ArrayList<Answer>();
+    
+    for (int i = 0; i < 200; i++) {
+      
+      int age = ThreadLocalRandom.current().nextInt(7, 91);; // 8 ate 90
+      int hate = ThreadLocalRandom.current().nextInt(0, 11);; // 1 ate 10
+
+      result.add(new Answer(i, age, hate));   
+    }
+
+    return result;
   }
 
 }
