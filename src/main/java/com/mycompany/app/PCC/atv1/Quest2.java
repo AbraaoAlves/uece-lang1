8b8c15e0380de3679f8560a15839125b35e9634b
AbraaoAlves <abraao.teodosio@gmail.com>
2023-08-26 21:16:56 +0000

    feat(PCC.Quest2): init question

---
+++ src/main/java/com/mycompany/app/PCC/atv1/Quest2.java
@@ -0,0 +1,15 @@
+package com.mycompany.app.PCC.atv1;
+
+/**
+ * Faça um algoritmo que receba os seguintes dados de uma pessoa: 
+ * nome (texto), cpf (texto), sexo (caractere) 
+ * e o ano de nascimento (inteiro) e armazene-os em um registro. 
+ * 
+ * Em seguida,apresente o nome, o sexo e a idade dessa pessoa.
+ * 
+ */
+public class Quest2 {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
