fafc590848abcad834a8037f51b26e929cbd03f6
AbraaoAlves <abraao.teodosio@gmail.com>
2023-08-27 21:07:22 +0000

    feat(PCC.Quest2): implement question2

--- src/main/java/com/mycompany/app/PCC/atv1/Quest2.java
+++ src/main/java/com/mycompany/app/PCC/atv1/Quest2.java
@@ -1,5 +1,7 @@
 package com.mycompany.app.PCC.atv1;
 
+import java.util.Scanner;
+
 /**
  * Faça um algoritmo que receba os seguintes dados de uma pessoa: 
  * nome (texto), cpf (texto), sexo (caractere) 
@@ -9,7 +11,42 @@ package com.mycompany.app.PCC.atv1;
  * 
  */
 public class Quest2 {
+  static Scanner scan = new Scanner(System.in);
+
   public static void main(String[] args) {
-    System.out.println("hello world");
+    String nome = inputString("Digite o nome da pessoa: ");
+    String cpf = inputString("Digite o cpf da pessoa: ");
+    char sexo = inputChar("Digite (M) para masculino e (F) para feminino: ");
+    int anoNascimento = inputInt("Digite o ano de nascimento: ");
+
+    printPessoa(nome, cpf, sexo, anoNascimento);
+  }
+
+  private static String inputString(String message) {
+    System.out.println(message);
+    return scan.nextLine();
+  }
+
+  private static int inputInt(String message) {
+    System.out.println(message);
+     return scan.nextInt();
+  }
+
+  private static char inputChar(String message) {
+    System.out.println(message);
+     return scan.next().charAt(0);
+  }
+
+  private static void printPessoa(String nome,String  cpf, char sexo ,int ano) {
+    System.out.println();
+    System.out.println("As informa\u00E7\u00F5es pessoais digitadas foram: ");
+    System.out.println();
+    System.out.println();
+
+    System.out.println("Nome: " + nome);
+    System.out.println("CPF: " + cpf);
+    System.out.println("Sexo: " + (sexo == 'M' ? "Masculino" : "Feminino"));
+    System.out.println("Ano: " + ano);
+    System.out.println();    
   }
 }
