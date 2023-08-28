05ca126074d23d6755d23935608497418eca0800
AbraaoAlves <abraao.teodosio@gmail.com>
2023-08-28 15:19:58 +0000

    feat(pcc.quest3): add scan operation

--- src/main/java/com/mycompany/app/PCC/atv1/Quest3.java
+++ src/main/java/com/mycompany/app/PCC/atv1/Quest3.java
@@ -1,5 +1,7 @@
 package com.mycompany.app.PCC.atv1;
 
+import java.util.Scanner;
+
 /**
  * 
  * Faça um algoritmo chamado calculadora, que exiba as opções de operações 
@@ -9,10 +11,34 @@ package com.mycompany.app.PCC.atv1;
  * Cada operação deverá ser criada em um procedimento (sub-rotina).
  */
 public class Quest3 {
+   
+  static Scanner scan = new Scanner(System.in);
 
   public static void main(String[] args) {
     System.out.println("System calc");
     System.out.println();
 
+    char operation = readOperation();
+    System.out.println("Operação escolhida: " + operation);
   }
+
+  private static char readOperation(){
+    System.out.println("Escolha uma operação valida para começar: ");
+    System.out.println();
+    System.out.println("(+) para soma");
+    System.out.println("(-) para subtração");
+    System.out.println("(*) para multiplicação");
+    System.out.println("(/) para divisão");
+    System.out.println();
+
+    char operation = scan.next().charAt(0);
+    String operations = "+-*/";
+    
+    if (!operations.contains(operations)) {
+      return readOperation();  
+    } else {
+      return operation;
+    }
+  }
+
 }
