7a8d1f6126634e68c380980dbe31521d4c4677d7
AbraaoAlves <abraao.teodosio@gmail.com>
2023-08-28 15:18:59 +0000

    feat(pcc.quest3): init

---
+++ src/main/java/com/mycompany/app/PCC/atv1/Quest3.java
@@ -0,0 +1,18 @@
+package com.mycompany.app.PCC.atv1;
+
+/**
+ * 
+ * Faça um algoritmo chamado calculadora, que exiba as opções de operações 
+ * (soma, subtração, multiplicação e divisão). 
+ * 
+ * O usuário deve escolher a operação e fornecer dois números. 
+ * Cada operação deverá ser criada em um procedimento (sub-rotina).
+ */
+public class Quest3 {
+
+  public static void main(String[] args) {
+    System.out.println("System calc");
+    System.out.println();
+
+  }
+}
