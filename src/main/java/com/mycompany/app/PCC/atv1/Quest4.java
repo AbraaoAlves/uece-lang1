c1fb3430e0e14edd4507d13091b2a9b6791a166d
AbraaoAlves <abraao.teodosio@gmail.com>
2023-08-28 21:06:41 +0000

    feact(pcc.quest4): init it

---
+++ src/main/java/com/mycompany/app/PCC/atv1/Quest4.java
@@ -0,0 +1,17 @@
+package com.mycompany.app.PCC.atv1;
+
+/**
+ * Faça um programa que armazene 
+ * o nome (texto), 
+ * a quantidade de créditos (inteiro) e 
+ * a quantidade de horas (inteiro) de 5 disciplinas e 
+ * 
+ * armazene-as em um arquivo binário. 
+ * 
+ * Em seguida, apresente os dados de cada disciplina armazenados nesse arquivo
+ */
+public class Quest4 {
+  public static void main(String[] args) {
+    System.out.println("Hello wolrd");
+  }
+}
