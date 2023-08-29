03d62c1381c8d9c88d2cf135cda5d3325f5f25e8
AbraaoAlves <abraao.teodosio@gmail.com>
2023-08-29 23:34:55 +0000

    feat(DisplinaIO): implement write/read object list in file

--- src/main/java/com/mycompany/app/PCC/atv1/quest4/DisciplinaIO.java
+++ src/main/java/com/mycompany/app/PCC/atv1/quest4/DisciplinaIO.java
@@ -2,6 +2,8 @@ package com.mycompany.app.PCC.atv1.quest4;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class DisciplinaIO {
@@ -17,14 +19,41 @@ public class DisciplinaIO {
     try {
       this.inputStream = new FileInputStream(file);
       this.outputStream = new FileOutputStream(file);
+
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
   }
-  public void write(List<Disciplina> list){
 
+  public void appendWrite(List<Disciplina> list) throws IOException, ClassNotFoundException{
+    List<Disciplina> old = new ArrayList<Disciplina>(this.read());  
+    old.addAll(list);
+
+    this.write(old);
   }
-  public List<Disciplina> read(){
-    return new ArrayList<Disciplina>();
+
+
+  public void write(List<Disciplina> list) throws IOException{    
+    ObjectOutputStream dos = new ObjectOutputStream(this.outputStream);
+
+    dos.writeObject(list);    
+    dos.close();
+  }
+
+  public List<Disciplina> read() throws IOException, ClassNotFoundException {    
+    ObjectInputStream fis = new ObjectInputStream(this.inputStream);
+    List<Disciplina> result = convertObjectToList(fis.readObject());
+
+    return result;   
+  }
+
+  private static List<Disciplina> convertObjectToList(Object obj) {
+    List<Disciplina> list = new ArrayList<Disciplina>();
+    if (obj.getClass().isArray()) {
+      list = Arrays.asList((Disciplina[])obj);
+    } else if (obj instanceof Collection) {
+      list = new ArrayList<Disciplina>((Collection<Disciplina>)obj);
+    }
+    return list;
   }
 }
