215db288384d159a6b0f6d022f9717f8a5068912
AbraaoAlves <abraao.teodosio@gmail.com>
2023-09-09 23:50:45 +0000

    feat(pcc.atv2.q1) add q1

---
+++ src/main/java/com/mycompany/app/PCC/atv2/Quest1.java
@@ -0,0 +1,26 @@
+gpackage com.mycompany.app.PCC.atv2;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+/**
+ * 
+ *  Proponha um algoritmo que leia um número inteiro n (1 n 100) 
+ * e gere n valores inteiros de forma aleatória, no intervalo de 1 a 100, 
+ * e insira um a um em um vetor V, de modo que, após a inserção de cada valor, 
+ * o vetor fique sempre ordenado de forma crescente. 
+ */
+public class Quest1 {
+  public static void main(String[] args) {
+    int n = new SecureRandom().nextInt(1, 100);
+    int[] vetor = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      vetor[i] = new SecureRandom().nextInt(1, 100);
+    }
+
+    Arrays.sort(vetor);
+
+    System.out.printf("Sorted vetor[] : %s", Arrays.toString(vetor));    
+  }
+}
